b033dc36b48c8e894a4835ca26df776d878ce750
nihao <dayurey@163.com>
2018-08-18 17:39:17 +0800

    登录页面 qianlei

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 import views
 
 urlpatterns=[
+    url(r'^$',views.doLogin),
     url(r'^fist/$',views.fist_view),
     url(r'^fist/top.html/$',views.top_view),
     url(r'^fist/left.html/$',views.left_view),
